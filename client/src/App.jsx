10a8c4fab22ab5604a5e0cdb66eab1801ec45bd5
Kirana Trupthi Shree <kiranats2006@gmail.com>
2025-08-06 12:21:45 +0530

    Done with registeration page

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register/>}></Route>
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
